3b4aab043568c67f6b337b15c81c388a0696523b
Matthew Loewen <matthewlanimates@gmail.com>
2019-12-11 18:47:10 -0500

    ultrasonic sensor
    completed arduino code

---
+++ Arduino/ultra-sonic/ultra-sonic.ino
@@ -0,0 +1,36 @@
+const int blueButton = 6;
+const int redButton = 5;
+
+const int trigPin = 9;
+const int echoPin = 10;
+
+float duration, distance;
+
+void setup() {
+  Serial.begin(9600);
+
+  // Define pin #12 as input and activate the internal pull-up , same for 13
+  pinMode(blueButton, INPUT_PULLUP);
+  pinMode(redButton, INPUT_PULLUP);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  int blueButtonValue = digitalRead(blueButton);
+  int redButtonValue = digitalRead(redButton);
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration * .0343) / 2;
+
+
+  Serial.println("blue: " + String(blueButtonValue) + " red: " + String(redButtonValue) + " sonic: " + distance);
+
+}
---
+++ main.py
@@ -0,0 +1,5 @@
+# author = matthew loewen
+# date = 12/11/2019
+# description = sie 558 final
+# this project reads data from 4 Arduinos with sensors and makes a visual display of their readings
+
