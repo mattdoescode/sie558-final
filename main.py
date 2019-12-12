4b06951fa3448d1772964da26a5b80ffb277b4a7
Matthew Loewen <matthewlanimates@gmail.com>
2019-12-12 02:06:02 -0500

    v1 color recording

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ textPositionSensor4 = [25, 600]
 pygame.init()
 white = (0, 0, 0)
 
-gameDisplay = pygame.display.set_mode((1000, 1000))
+screenX = screenY = 1000
+
+gameDisplay = pygame.display.set_mode((screenX, screenY))
 pygame.display.set_caption('Matthew Loewen - SIE 558 Final')
 
 crashed = False
@@ -28,18 +30,22 @@ def text_objects(text, font):
     textSurface = font.render(text, True, white)
     return textSurface
 
+
 def messageDisplay(text, size, xPos, Ypos):
     largeText = pygame.font.SysFont("C:\Windows\Fonts\Arial.ttf", size)
     textSurf = text_objects(text, largeText)
     gameDisplay.blit(textSurf, (xPos, Ypos))
 
-#
-# Function from:
+# # Function from:
 # https://stackoverflow.com/questions/929103/convert-a-number-range-to-another-range-maintaining-ratio
 def translateValueRange(value, oldMin, oldMax, newMin, newMax):
-    newValue = (((value - oldMin) * (newMax - newMin)) / (oldMax - oldMin)) + newMin
-    return newValue;
+   newValue = (((value - oldMin) * (newMax - newMin)) / (oldMax - oldMin)) + newMin
+   return newValue;
 
+micIdeal = 24
+# determine error ranges
+idealLight = 630
+distanceIdeal = 10.1;
 
 while not crashed:
     for event in pygame.event.get():
@@ -48,35 +54,65 @@ while not crashed:
 
     gameDisplay.fill((255, 255, 255))
 
-    red1, blue1, red2, blue2, red3, blue3, red4, blue4, pot, photo, mic, distance = arduino1.readline().decode('ascii').split(",")
+    red1, blue1, red2, blue2, red3, blue3, red4, blue4, pot, photo, mic, distance = arduino1.readline().decode(
+        'ascii').split(",")
 
-    # draw 4 squares 1 for each sensor.
-    pygame.draw.rect(gameDisplay, (255, 0, 0), (0, 0, 300, 300))
+    # microphone color square
+    mic = int(mic)
+    if mic == micIdeal:
+        pygame.draw.rect(gameDisplay, (0, 255, 0), (0, 0, screenX/2, screenY/2))
+    elif mic == micIdeal + 1 or mic == micIdeal - 1 or mic == micIdeal - 2:
+        pygame.draw.rect(gameDisplay, (125, 130, 0), (0, 0, screenX/2, screenY/2))
+    else:
+        pygame.draw.rect(gameDisplay, (255, 0, 0), (0, 0, screenX/2, screenY/2))
 
     # display grid 1
     messageDisplay("Station 1:", 48, textPositionSensor1[0], textPositionSensor1[1])
     messageDisplay("Blue Button: " + blue1, 36, textPositionSensor1[0], textPositionSensor1[1] + 35)
     messageDisplay("Red Button: " + red1, 36, textPositionSensor1[0], textPositionSensor1[1] + 70)
-    messageDisplay("Microphone: " + mic, 36, textPositionSensor1[0], textPositionSensor1[1] + 105)
+    messageDisplay("Microphone: " + str(mic), 36, textPositionSensor1[0], textPositionSensor1[1] + 105)
 
-    # value of mic is 24ish
-    # we have noise if it gets above 24
-    
+    # removed ascii characters
+    distance = distance.rstrip()
+
+    if float(distance) > 20:
+        changeColor = 255
+    elif float(distance) < 10:
+        changeColor = 255
+    else:
+        changeColor = translateValueRange(float(distance), 10, 20, 0, 255)
+
+    pygame.draw.rect(gameDisplay, (changeColor, 255 - changeColor, 0), (screenX/2, 0, screenX/2, screenY/2))
+
 
     # display grid 2
     messageDisplay("Station 2:", 48, textPositionSensor2[0], textPositionSensor2[1])
     messageDisplay("Blue Button: " + blue2, 36, textPositionSensor2[0], textPositionSensor2[1] + 35)
     messageDisplay("Red Button: " + red2, 36, textPositionSensor2[0], textPositionSensor2[1] + 70)
-    distance = distance.rstrip()
     messageDisplay("Distance : " + distance, 36, textPositionSensor2[0], textPositionSensor2[1] + 105)
 
-    #display grid 3
+    lightError = abs(idealLight - int(photo))
+    if lightError > 255:
+        lightError = 255
+
+    pygame.draw.rect(gameDisplay, (lightError, 255 - lightError, 0), (screenX/2, screenY/2, screenX/2, screenY/2))
+
+    # display grid 3
     messageDisplay("Station 3:", 48, textPositionSensor3[0], textPositionSensor3[1])
     messageDisplay("Blue Button: " + blue3, 36, textPositionSensor3[0], textPositionSensor3[1] + 35)
     messageDisplay("Red Button: " + red3, 36, textPositionSensor3[0], textPositionSensor3[1] + 70)
     messageDisplay("Light Level: " + photo, 36, textPositionSensor3[0], textPositionSensor3[1] + 105)
 
-    #display grid 4
+    if int(pot) <= 300:
+        potError = 255
+        pygame.draw.rect(gameDisplay, (255, 0, 0), (0, screenY / 2, screenX / 2, screenY / 2))
+    else:
+        potError = translateValueRange(int(pot), 300, 1024, 0, 255)
+        pygame.draw.rect(gameDisplay, (255 - potError, potError, 0), (0, screenY / 2, screenX / 2, screenY / 2))
+
+    print(potError)
+
+    # display grid 4
     messageDisplay("Station 4:", 48, textPositionSensor4[0], textPositionSensor4[1])
     messageDisplay("Blue Button: " + blue4, 36, textPositionSensor4[0], textPositionSensor4[1] + 35)
     messageDisplay("Red Button: " + red4, 36, textPositionSensor4[0], textPositionSensor4[1] + 70)
